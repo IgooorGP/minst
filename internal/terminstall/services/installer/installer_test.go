ca190cf0661ea82bfe3aa2ca63e9494dc0a899f5
IgooorGP <igor.feq@gmail.com>
2020-08-10 16:37:15 -0300

    Added tests for building install commands

--- internal/terminstall/services/installer/installer_test.go
+++ internal/terminstall/services/installer/installer_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const testDataDirectory = "../../../../test/data/test_install_v1.yml"
+const testDataDirectory = "../../../../test/data/test_install_less_apps.yaml"
 
 func TestShouldGetProvidersForInstallFile(t *testing.T) {
 	// arrange
@@ -19,10 +19,10 @@ func TestShouldGetProvidersForInstallFile(t *testing.T) {
 	// assert
 	assert.Len(t, providers, 2) // 2 providers
 	assert.Equal(t, providers[0].Name, "brew")
-	assert.Equal(t, providers[0].BaseCommand, "brew")
+	assert.Equal(t, providers[0].BaseCommand, "brew install")
 
 	assert.Equal(t, providers[1].Name, "brewcask")
-	assert.Equal(t, providers[1].BaseCommand, "brew cask")
+	assert.Equal(t, providers[1].BaseCommand, "brew cask install")
 }
 
 func TestGetInstallCommandsForProvider(t *testing.T) {
@@ -37,4 +37,16 @@ func TestGetInstallCommandsForProvider(t *testing.T) {
 	// assert
 	assert.Equal(t, brewCaskProvider.Name, "brewcask")
 	assert.Len(t, installCommands, 2)
+
+	// assert - for brew cask install slack
+	assert.Equal(t, installCommands[0].Command, "brew")
+	assert.Equal(t, installCommands[0].CommandArgs, []string{"cask", "install", "slack"})
+	assert.Equal(t, installCommands[0].Executed, false)
+	assert.Equal(t, installCommands[0].HasError, false)
+
+	// assert - for brew cask install vscode
+	assert.Equal(t, installCommands[1].Command, "brew")
+	assert.Equal(t, installCommands[1].CommandArgs, []string{"cask", "install", "vscode"})
+	assert.Equal(t, installCommands[1].Executed, false)
+	assert.Equal(t, installCommands[1].HasError, false)
 }
